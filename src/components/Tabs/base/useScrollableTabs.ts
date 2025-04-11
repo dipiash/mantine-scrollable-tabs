fbb2aaee52dd58c1f635ab72aeb5a0a54b0ee1b0
Dmitriy Pashkevich <dmitriytemn@gmail.com>
2025-04-11 21:06:39 +0300

    Add scroll on render immediately to active tab. Add scroll on resize

--- src/components/Tabs/base/useScrollableTabs.ts
+++ src/components/Tabs/base/useScrollableTabs.ts
@@ -1,21 +1,26 @@
 import { useEffect, useRef } from "react";
+import { rafThrottle } from "../../../shared/utils/rafThrottle";
+
+const ACTIVE_TAB_SELECTOR = '[aria-selected="true"]'
+
+const scrollToActiveTab = (element?: HTMLDivElement) => {
+    const activeTab = element?.querySelector<HTMLButtonElement>(ACTIVE_TAB_SELECTOR);
+
+    if (activeTab) {
+        activeTab.scrollIntoView({ behavior: "smooth", inline: "center", block: "nearest" });
+    }
+};
 
 export const useScrollableTabs = () => {
-    const tabListReference = useRef<HTMLDivElement | null>(null);
+    const tabListReference = useRef<HTMLDivElement | undefined>(undefined);
 
     useEffect(() => {
-        const observer = new MutationObserver(() => {
-            const activeTab = tabListReference.current?.querySelector<HTMLButtonElement>(
-                '[aria-selected="true"]'
-            );
+        // Scroll immediately after mounting
+        scrollToActiveTab(tabListReference.current);
 
-            if (activeTab) {
-                activeTab.scrollIntoView({
-                    behavior: "smooth",
-                    inline: "center",
-                    block: "nearest",
-                });
-            }
+        // Scroll when active
+        const observer = new MutationObserver(() => {
+            scrollToActiveTab(tabListReference.current);
         });
 
         if (tabListReference.current) {
@@ -25,7 +30,17 @@ export const useScrollableTabs = () => {
             });
         }
 
-        return () => observer.disconnect();
+        // Add resize handler
+        const throttledHandleResize = rafThrottle(() => {
+            scrollToActiveTab(tabListReference.current);
+        });
+
+        window.addEventListener("resize", throttledHandleResize);
+
+        return () => {
+            observer.disconnect();
+            window.removeEventListener("resize", throttledHandleResize);
+        }
     }, []);
 
     return {
